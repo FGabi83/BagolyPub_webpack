e74a57b57bee9c03c56482e4b91e3cc0f56901da
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-04-15 13:24:59 +0200

    htmls moved to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-/*const CopyWebpackPlugin = require("copy-webpack-plugin");*/
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
@@ -43,21 +42,6 @@ const styles = {
   ], 
 };
 
-/*const images = {
-  test: /\.(png|jpe?g|gif|svg)$/i,
-  loader: "file-loader",
-  options: {
-    name: "images/photos/[name].[ext]",
-  },
-};
-
-const icons = {
-  test: /\.svg$/,
-  loader: "file-loader",
-  options: {
-    name: "images/icons/[name].[ext]", // Így fogja a fájlokat a dist/images/icons mappába helyezni
-  },
-};*/
 
 const pug = {
   test: /\.pug$/,
@@ -86,41 +70,27 @@ const config = {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "style.css" }),
-    /*new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "./public/images/photos", // forrás mappa
-          to: "images/photos", // cél mappa a dist mappában
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "./public/images/icons",
-          to: "images/icons",
-        },
-      ],
-    }),*/
+    
+    
     new HtmlWebpackPlugin({
       template: "./views/index.pug",
-      filename: "index.html", 
+      filename: "../index.html", 
     }),
     new HtmlWebpackPlugin({
       template: "./views/drinks.pug",
-      filename: "kinalat.html", 
+      filename: "../kinalat.html", 
     }),
     new HtmlWebpackPlugin({
       template: "./views/contactPage.pug",
-      filename: "kapcsolat.html", 
+      filename: "../kapcsolat.html", 
     }),
     new HtmlWebpackPlugin({
       template: "./views/privacyPolicy.pug",
-      filename: "adatvedelem.html", 
+      filename: "../adatvedelem.html", 
     }),
     new HtmlWebpackPlugin({
       template: "./views/impressium.pug",
-      filename: "impresszium.html", 
+      filename: "../impresszium.html", 
     }),
   ],
 };
